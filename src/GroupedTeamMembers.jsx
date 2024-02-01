cdc5da6c905a54f8e6ccccec61a9452a5ce78119
RishithaVanamala <rishithavanamala@gmail.com>
2024-02-01 13:33:42 -0500

    Team Member Allocation Website

--- src/GroupedTeamMembers.jsx
+++ src/GroupedTeamMembers.jsx
@@ -58,6 +58,7 @@ const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
                                 })
                             }
                         </div>
+                        <hr/>
                         </div>
                     )
                 })
